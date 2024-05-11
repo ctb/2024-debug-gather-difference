3873140e565b2fb520c4c6d1e753429f01bc1e7e
C. Titus Brown <titus@idyll.org>
2024-05-11 05:28:30 -0700

    upd

---
+++ run.sh
@@ -0,0 +1,12 @@
+# commands run for each variant:
+
+sourmash gather SRR606249.trim.k31.sig.zip combined-matches-k31.sig.zip -o SRR606249.x.combined-matches.gather.csv
+
+#sourmash scripts index -k 31 -m DNA -o combined-matches-k31.rocksdb combined-matches-k31.sig.zip
+
+#sourmash scripts fastmultigather SRR606249.trim.k31.sig.zip combined-matches-k31.rocksdb -o srr.fmg-rdb.csv 2> srr.fmg-rdb.out
+
+sourmash scripts fastgather SRR606249.trim.k31.sig.zip combined-matches-k31.sig.zip -o srr.fg.csv
+
+sourmash scripts fastmultigather SRR606249.trim.k31.sig.zip combined-matches-k31.sig.zip 2> srr.fmg.out # -o srr.fmg.csv
+mv SRR606249.gather.csv srr.fmg.csv
